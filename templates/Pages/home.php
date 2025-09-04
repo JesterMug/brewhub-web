e7b6fee7830693e2db56457b9455669240059260
samford <sfor0027@student.monash.edu>
2025-09-04 17:57:02 +1000

    - Added Content Management System

--- templates/Pages/home.php
+++ templates/Pages/home.php
@@ -81,6 +81,7 @@ endif;
     <div class="hero-overlay">
         <h1>Welcome to <span class="highlight">BrewHub</span></h1>
         <p>Crafting coffee of outstanding quality.</p>
+        <?= $this->Html->link('Content Blocks', ['plugin' => 'ContentBlocks', 'controller' => 'ContentBlocks', 'action' => 'index']) ?>
     </div>
 </header>
 
