209ea982b6fc09dd7060cd295d57913e47cdba23
SHULI YE <syee0026@student.monash.edu>
2025-09-09 01:21:43 +1000

    add new register common and let it land on homepage and log out

--- templates/element/navigation.php
+++ templates/element/navigation.php
@@ -4,9 +4,34 @@
     <div class="container">
         <a href="<?= $this->Url->build('/') ?>" class="brand">BrewHub</a>
         <ul class="nav-links">
-            <li><a href="<?= $this->Url->build(['controller' => 'forms', 'action' => 'index']) ?>">Login</a></li>
-            <li><a href="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'display', 'index']) ?>">Shop</a></li>
-            <li><a href="<?= $this->Url->build(['controller' => 'forms', 'action' => 'add']) ?>" class="btn btn-outline">Get in Touch</a></li>
+            <?php if ($this->Identity->isLoggedIn()): ?>
+                <?php if (in_array($this->Identity->get('user_type'), ['admin', 'superuser'])): ?>
+                    <!-- Admin or Superuser: show Dashboard and Logout -->
+                    <li>
+                        <a href="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'dashboard']) ?>">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'logout']) ?>">Logout</a>
+                    </li>
+                <?php else: ?>
+                    <!-- Customer: show Logout only -->
+                    <li>
+                        <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'logout']) ?>">Logout</a>
+                    </li>
+                <?php endif; ?>
+            <?php else: ?>
+                <!-- Not logged in: show Login -->
+                <li>
+                    <a href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'login']) ?>">Login</a>
+                </li>
+            <?php endif; ?>
+
+            <li>
+                <a href="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'display', 'index']) ?>">Shop</a>
+            </li>
+            <li>
+                <a href="<?= $this->Url->build(['controller' => 'Forms', 'action' => 'add']) ?>" class="btn btn-outline">Get in Touch</a>
+            </li>
         </ul>
     </div>
 </nav>
