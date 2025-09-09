508e9ee2feb7bf8569983c8f61873e3ca8c126aa
Jagannath Ayinipully <jayi0001@student.monash.edu>
2025-09-10 03:05:25 +1000

    Fixed Controllers that reverted during a merge.

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -44,15 +44,46 @@ class ProductsController extends AppController
     public function add()
     {
         $product = $this->Products->newEmptyEntity();
-        if ($this->request->is('post')) {
-            $product = $this->Products->patchEntity($product, $this->request->getData());
-            if ($this->Products->save($product)) {
-                $this->Flash->success(__('The product has been saved.'));
 
+        if ($this->request->is('post')) {
+            $data = $this->request->getData();
+
+            // Extract uploaded files
+            $uploadedFiles = $this->request->getData('product_images_files');
+            unset($data['product_images_files']);
+
+            $product = $this->Products->patchEntity($product, $data, [
+                'associated' => ['ProductMerchandise', 'ProductVariants', 'ProductCoffee']
+            ]);
+
+            if ($this->Products->save($product)) {
+                $productId = $product->id;
+
+                //Handle image uploads separately
+                if (!empty($uploadedFiles[0])) {
+                    foreach ($uploadedFiles as $file) {
+                        if ($file && $file->getError() === UPLOAD_ERR_OK) {
+                            $originalName = preg_replace('/[^A-Za-z0-9._-]/', '_', $file->getClientFilename());
+                            $extension = pathinfo($originalName, PATHINFO_EXTENSION);
+                            $uniqueName = uniqid('img_', true) . '.' . strtolower($extension);
+
+                            $file->moveTo(WWW_ROOT . 'img/products/' . $uniqueName);
+
+                            $image = $this->Products->ProductImages->newEmptyEntity();
+                            $image->product_id = $product->id;
+                            $image->image_file = $uniqueName;
+                            $this->Products->ProductImages->save($image);
+                        }
+                    }
+                }
+
+                $this->Flash->success(__('The product has been saved.'));
                 return $this->redirect(['action' => 'index']);
             }
+
             $this->Flash->error(__('The product could not be saved. Please, try again.'));
         }
+
         $this->set(compact('product'));
     }
 
