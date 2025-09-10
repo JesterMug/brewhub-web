ed13e04a43b52311725245927bd93ff4262dec4d
SHULI YE <syee0026@student.monash.edu>
2025-09-10 20:14:54 +1000

    add logout button at projects

--- templates/element/navigation.php
+++ templates/element/navigation.php
@@ -1,4 +1,5 @@
 <?php
+$user = $this->request->getAttribute('identity');
 ?>
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container px-4 px-lg-5">
@@ -10,20 +11,35 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ms-auto">
 
+                <?php if ($user): ?>
+                    <?php if (in_array($user->user_type, ['admin', 'superuser'])): ?>
+                        <!-- Admin or Superuser: show Dashboard and Logout -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= $this->Url->build(['controller' => 'Pages', 'action' => 'dashboard']) ?>">Dashboard</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'logout']) ?>">Logout</a>
+                        </li>
+                    <?php else: ?>
+                        <!-- Customer: show Logout only -->
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'logout']) ?>">Logout</a>
+                        </li>
+                    <?php endif; ?>
+                <?php else: ?>
+                    <!-- Not logged in: show Login -->
+                    <li class="nav-item">
+                        <a class="nav-link" href="<?= $this->Url->build(['controller' => 'Users', 'action' => 'login']) ?>">Log in</a>
+                    </li>
+                <?php endif; ?>
+
                 <li class="nav-item">
-                <a class="nav-link" href="<?= $this->Url->build(['controller' => 'forms', 'action' => 'index']) ?>">Log in</a>
+                    <a class ="nav-link" href="<?= $this->Url->build(['controller' => 'forms', 'action' => 'add']) ?>">Shop</a>
                 </li>
-
-
-                <li class="nav-item">
-                <a class ="nav-link" href="<?= $this->Url->build(['controller' => 'forms', 'action' => 'add']) ?>">Shop</a>
-                </li>
-
 
                 <li class="nav-item">
                     <a class ="nav-link" href="<?= $this->Url->build(['controller' => 'forms', 'action' => 'add']) ?>">Get in Touch</a>
                 </li>
-
 
             </ul>
         </div>
