57e1cc3717f0e444f413f379104035656db601a8
SHULI YE <syee0026@student.monash.edu>
2025-09-11 17:05:06 +1000

    add trim white space in user names

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -9,14 +9,14 @@ use Authentication\PasswordHasher\DefaultPasswordHasher;
 class User extends Entity
 {
     protected array $_accessible = [
-        'first_name' => true,
-        'last_name'  => true,
-        'email'      => true,
-        'password'   => true,
-        'user_type'  => true,
-        'nonce' => true,
-        'nonce_expiry' => true,
-        'date_created' => true,
+        'first_name'    => true,
+        'last_name'     => true,
+        'email'         => true,
+        'password'      => true,
+        'user_type'     => true,
+        'nonce'         => true,
+        'nonce_expiry'  => true,
+        'date_created'  => true,
         'date_modified' => true,
     ];
 
@@ -28,5 +28,23 @@ class User extends Entity
             return null;
         }
         return (new DefaultPasswordHasher())->hash($password);
+    }
+
+    // 🔹 Automatically trim first name
+    protected function _setFirstName(?string $firstName): ?string
+    {
+        return $firstName !== null ? trim($firstName) : null;
+    }
+
+    // 🔹 Automatically trim last name
+    protected function _setLastName(?string $lastName): ?string
+    {
+        return $lastName !== null ? trim($lastName) : null;
+    }
+
+    // (Optional) 🔹 Also trim email to avoid spaces around it
+    protected function _setEmail(?string $email): ?string
+    {
+        return $email !== null ? trim($email) : null;
     }
 }
