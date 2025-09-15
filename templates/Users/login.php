af4682d8f60e8b2ce5fb72fd21cdb6d80bd6938a
dtra0050 <dtra0050@student.monash.edu>
2025-09-15 17:34:40 +1000

    - Changed Login and Users page layout - Changed Card containers layout and forms content layout to fit visuals better

--- templates/Users/login.php
+++ templates/Users/login.php
@@ -4,15 +4,26 @@ $this->assign('title', 'Login');
 ?>
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="col-xl-10 col-lg-6 col-md-9">
+
+
+            <!-- Card Begins -->
+
+
+            <div class="card o-hidden border-0 shadow-lg my-5" style="min-height: 400px:">
                 <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
+                    <div class="row h-100">
+                        <div class="col-12 d-flex justify-content-center align-items-center">
+                            <div class="p-5 w-100 text-center">
+
+
+
+                                <!-- Form Beings -->
+
+
                                 <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Welcome back</h1>
+                                    <h1 class="h4 text-gray-900 mb-4" style="font-size: 30px">Welcome back</h1>
+                                    <h4 class="text-gray-500 mb-4" style="font-size: 12px">Sign in to your account here</h4>
                                 </div>
                                 <?= $this->Form->create(null, ['class' => 'user']) ?>
                                 <div class="form-group">
--- templates/Users/register.php
+++ templates/Users/register.php
@@ -5,74 +5,84 @@ $this->assign('title', 'Register');
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-xl-10 col-lg-12 col-md-9">
-            <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="col-xl-10 col-lg-6 col-md-9">
+
+
+            <!-- Card Begins -->
+
+
+
+            <div class="card o-hidden border-0 shadow-lg my-5" style="min-height: 400px:>
                 <div class="card-body p-0">
-                    <div class="row">
-                        <div class="col-lg-6 d-none d-lg-block bg-register-image"></div>
-                        <div class="col-lg-6">
-                            <div class="p-5">
-                                <div class="text-center">
-                                    <h1 class="h4 text-gray-900 mb-4">Join Now</h1>
-                                </div>
+            <div class="row h-100">
+                <div class="col-12 d-flex justify-content-center align-items-center">
+                    <div class="p-5 w-100 text-center">
 
-                                <?= $this->Form->create($user, ['class' => 'user']) ?>
 
-                                <div class="form-group">
-                                    <?= $this->Form->control('first_name', [
-                                        'label' => false,
-                                        'class' => 'form-control form-control-user',
-                                        'placeholder' => 'First Name',
-                                        'required' => true
-                                    ]) ?>
-                                </div>
+                        <!-- Form Beings -->
 
-                                <div class="form-group">
-                                    <?= $this->Form->control('last_name', [
-                                        'label' => false,
-                                        'class' => 'form-control form-control-user',
-                                        'placeholder' => 'Last Name',
-                                        'required' => true
-                                    ]) ?>
-                                </div>
 
-                                <div class="form-group">
-                                    <?= $this->Form->control('email', [
-                                        'label' => false,
-                                        'class' => 'form-control form-control-user',
-                                        'placeholder' => 'Email Address',
-                                        'required' => true
-                                    ]) ?>
-                                </div>
+                        <div class="text-center">
+                            <h1 class="h4 text-gray-900 mb-4" style="font-size: 30px">Join Now</h1>
+                        </div>
 
-                                <div class="form-group">
-                                    <?= $this->Form->control('password', [
-                                        'label' => false,
-                                        'class' => 'form-control form-control-user',
-                                        'placeholder' => 'Password',
-                                        'type' => 'password',
-                                        'required' => true
-                                    ]) ?>
-                                </div>
+                        <?= $this->Form->create($user, ['class' => 'user']) ?>
 
-                                <div class="d-grid">
-                                    <button class="btn btn-primary btn-user btn-block">Register</button>
-                                </div>
+                        <div class="form-group">
+                            <?= $this->Form->control('first_name', [
+                                'label' => false,
+                                'class' => 'form-control form-control-user',
+                                'placeholder' => 'First Name',
+                                'required' => true
+                            ]) ?>
+                        </div>
 
-                                <?= $this->Form->end() ?>
+                        <div class="form-group">
+                            <?= $this->Form->control('last_name', [
+                                'label' => false,
+                                'class' => 'form-control form-control-user',
+                                'placeholder' => 'Last Name',
+                                'required' => true
+                            ]) ?>
+                        </div>
 
-                                <hr>
-                                <div class="text-center">
-                                    <?= $this->Html->link('Already have an account? Login!', ['controller' => 'Users', 'action' => 'login'], ['class' => 'small']) ?>
-                                </div>
-                                <div class="text-center">
-                                    <?= $this->Html->link('Go to Homepage', '/', ['class' => 'small']) ?>
-                                </div>
-                            </div>
+                        <div class="form-group">
+                            <?= $this->Form->control('email', [
+                                'label' => false,
+                                'class' => 'form-control form-control-user',
+                                'placeholder' => 'Email Address',
+                                'required' => true
+                            ]) ?>
+                        </div>
+
+                        <div class="form-group">
+                            <?= $this->Form->control('password', [
+                                'label' => false,
+                                'class' => 'form-control form-control-user',
+                                'placeholder' => 'Password',
+                                'type' => 'password',
+                                'required' => true
+                            ]) ?>
+                        </div>
+
+                        <div class="d-grid">
+                            <button class="btn btn-primary btn-user btn-block">Register</button>
+                        </div>
+
+                        <?= $this->Form->end() ?>
+
+                        <hr>
+                        <div class="text-center">
+                            <?= $this->Html->link('Already have an account? Login!', ['controller' => 'Users', 'action' => 'login'], ['class' => 'small']) ?>
+                        </div>
+                        <div class="text-center">
+                            <?= $this->Html->link('Go to Homepage', '/', ['class' => 'small']) ?>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+</div>
+</div>
 </div>
